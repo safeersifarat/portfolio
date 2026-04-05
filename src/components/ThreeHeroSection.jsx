e4fb5e8c8d4d9285de7180e84deee99b46f79577
safeer <safeersifarath@gmail.com>
2026-04-05 23:38:57 +0530

    again

--- src/components/ThreeHeroSection.jsx
+++ src/components/ThreeHeroSection.jsx
@@ -98,7 +98,7 @@ function RetroComputer({ scrollProgress = 0, displayedRole, windowWidth }) {
           {isPhone ? (
             <>
               <div className="flex justify-center items-center mb-4">
-                <div className="relative h-32 w-28 scale-x-350  border-white flex items-center justify-center">
+                <div className="relative h-32 w-32 scale-x-350  border-white flex items-center justify-center">
                   <img
                     src={profilePic}
                     alt="Safeer"
